fb82cb4ba35b8356828bfa8c3b60a3b02b517cb9
swxEmily <shiwenxiu@huawei.com>
2019-03-28 15:06:17 +0800

    Title:delete out path Description:

--- common/prepare_presenter_server.sh
+++ common/prepare_presenter_server.sh
@@ -143,7 +143,7 @@ function main()
 {
     stop_pid=`ps -ef | grep "presenter_server\.py" | grep "${app_name}" | awk -F ' ' '{print $2}'`
     if [[ ${stop_pid}"X" != "X" ]];then
-        echo "Kill existing presenter process: kill -9 ${stop_pid}"
+        echo -e "\033[33mNow do presenter server configuration, kill existing presenter process: kill -9 ${stop_pid}.\033[0m"
         kill -9 ${stop_pid}
     fi
 
